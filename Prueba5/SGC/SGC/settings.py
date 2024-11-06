4dd0358b06c72c40f7edcd3765f5c9ee0990b51b
aegonzalez73 <alejandro_e_gonzalez@yahoo.com.ar>
2024-11-06 08:18:09 -0300

    Revisión de vistas
    Views.py: Vistas revisadas y comentadas (código no usado eliminado)
    Tokens.py: agregado
    Settings.py: limpieza (falta MUCHO aun). email configurado para consola (para producción está comentado)
    urls.py: limpieza, también falta. Se elimina el urls.py dentro de App_back.
    mail_activacion.html: mensaje para el cuerpo del correo de activación

--- Prueba5/SGC/SGC/settings.py
+++ Prueba5/SGC/SGC/settings.py
@@ -101,7 +101,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'Prueba5_PSQL',
         'USER': 'postgres',
-        'PASSWORD': 'Avit3578', #'123Bbokari123',
+        'PASSWORD': 'Avit3578', 
         'HOST': 'localhost',  # o el host donde está tu base de datos
         'PORT': '5432',       # El puerto por defecto de PostgreSQL es 5432
     }
@@ -199,16 +199,17 @@ ACCOUNT_USERNAME_REQUIRED = False
 ACCOUNT_AUTHENTICATION_METHOD = 'email'
 ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
 
-# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'  # Para pruebas locales
+
+# Para pruebas en consola:
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'  # Para pruebas locales
 
 # Para producción (verificar)
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'ka.hinados02@gmail.com'
-EMAIL_HOST_PASSWORD = 'hlbvcbythgjcphmg'
+#EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+#EMAIL_HOST = 'smtp.gmail.com'
+#EMAIL_PORT = 587
+#EMAIL_USE_TLS = True
+#EMAIL_HOST_USER = 'ka.hinados02@gmail.com'
+#EMAIL_HOST_PASSWORD = 'hlbvcbythgjcphmg'
 
 
 LOGGING = {
